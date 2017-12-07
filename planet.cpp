273246b0f83cd129410c5f87837d5ffd3bda9e49
Andreas Boden <andbod@kth.se>
2017-12-07 12:09:50 +0100

    ..

---
+++ planet.cpp
@@ -0,0 +1,6 @@
+#include "planet.hpp"
+
+void Planet::Planet(glm::vec3 origin, float lin_vel, float ang_vel, int num_Fe_particles, int num_Si_particles){
+  
+
+}
--- planet.hpp
+++ planet.hpp
@@ -11,14 +11,15 @@ class Planet
 		float origin[3] = { 0, 0, 0 };
 		std::vector<Particle> silicateParticles;
 		std::vector<Particle> ironParticles;
-    		float colorSilicate[4] = {0, 1, 0, 0.4};
-    		float colorIron[4] = {0, 0, 1, 0.4};
+		float colorSilicate[4] = {0, 1, 0, 0.4};
+		float colorIron[4] = {0, 0, 1, 0.4};
 	public:
 		void setOrigin(double x, double y, double z) {
 			origin[0] = x; origin[1] = y; origin[2] = z;
 		};
-		void addSilicateParticle(double x, double y, double z) {
-			silicateParticles.push_back(glm::vec3(x + origin[0], y + origin[1], z + origin[2]));
+		void addSilicateParticle(double x_pos, double y_pos, double z_pos) {
+			silicateParticles.push_back(Particle{glm::vec3(x_pos + origin[0], y_pos + origin[1], z_pos + origin[2]), velocity, false});
+			// silicateParticles.push_back(glm::vec3(x + origin[0], y + origin[1], z + origin[2]));
 		};
 		void addIronParticle(double x, double y, double z) {
 			ironParticles.push_back(glm::vec3(x + origin[0], y + origin[1], z + origin[2]));
